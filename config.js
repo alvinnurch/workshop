697c8482c60d7d01b82d24edc3a8b736d53fa171
alvinnurch <alvinnurch@gmail.com>
2026-09-13 18:53:13 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -7,7 +7,7 @@
 /* Web app URL dari Apps Script.
    Contoh: "https://script.google.com/macros/s/AKfycb.../exec"
    Dibiarkan kosong = mode demo (data contoh tersimpan di peramban). */
-window.WORKSHOP_API = "";
+window.WORKSHOP_API = "https://script.google.com/macros/s/AKfycby3-NRtzflfGzqosLdw2AT-gmZSwpZdA6JM1sYyckalbL4BxeU90n_-Jnjlj7C8JxXE/exec";
 
 /* Harus sama dengan ADMIN_PASSWORD di apps-script/Code.gs */
 window.WORKSHOP_ADMIN_PASSWORD = "sayaadminnya";
